2f9d6d2e1af2083acd28f969de617eabca8ad968
Zack Tollman <tollmanz@gmail.com>
2015-09-15 11:44:44 -0700

    Test index type change.
    It appears that when meta_key is utf8, but is limited to 191 chars, the
    new query is slow and the old is fast. Perhaps the real issue has to do
    with the index char size?

---
+++ sql/createMetaKeyNotMB4IndexWith191.sql
@@ -0,0 +1,10 @@
+USE app;
+CREATE TABLE `wp_postmeta` (
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`meta_id`),
+  KEY `post_id` (`post_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=212769015 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
