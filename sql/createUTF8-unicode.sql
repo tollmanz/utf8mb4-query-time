904563cc814462e2ef95097ad7869e305cfc96fe
Zack Tollman <tollmanz@gmail.com>
2015-09-29 20:58:39 -0700

    Add missing file.

---
+++ sql/createUTF8-unicode.sql
@@ -0,0 +1,10 @@
+USE app;
+CREATE TABLE `wp_postmeta` (
+  `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) DEFAULT NULL,
+  `meta_value` longtext,
+  PRIMARY KEY (`meta_id`),
+  KEY `post_id` (`post_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=212732319 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;;
