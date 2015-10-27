b855e137a3c84777b43a811bd027721a8dd9d76b
Gary Pendergast <gary@pento.net>
2015-10-27 20:04:10 +1100

    Indexes need brackets.

--- sql/newQueryForceIndex.sql
+++ sql/newQueryForceIndex.sql
@@ -3,7 +3,7 @@ USE app;
 SET profiling = 1;
 
 SELECT DISTINCT meta_key
-FROM wp_postmeta FORCE INDEX meta_key
+FROM wp_postmeta FORCE INDEX (meta_key)
 WHERE meta_key NOT BETWEEN '_' AND '_z'
 HAVING meta_key NOT LIKE '\_%'
 ORDER BY meta_key
@@ -13,7 +13,7 @@ SHOW PROFILES;
 
 EXPLAIN
 SELECT DISTINCT meta_key
-FROM wp_postmeta FORCE INDEX meta_key
+FROM wp_postmeta FORCE INDEX (meta_key)
 WHERE meta_key NOT BETWEEN '_' AND '_z'
 HAVING meta_key NOT LIKE '\_%'
 ORDER BY meta_key
